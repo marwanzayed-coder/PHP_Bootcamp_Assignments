5ebe4cb0985b325e44c479ea48e5389f8dfb6fc6
marwanzayed-coder <marwanzayedshaban@gmail.com>
2022-04-06 23:13:35 +0200

    Edit index.php

--- index.php
+++ index.php
@@ -5,38 +5,84 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Bootcamp Assignments | Home Page</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400&display=swap" rel="stylesheet">
     <style>
-    	.week {
-    		padding: 20px 50px;
-    	}
-    	.week h2 {
-    		font-size: 35px;
-    		background: #ececec;
-    		padding: 20px;
-    		text-align: center;
-    	}
-    	.week ul {
-    		list-style: auto;
-    	}
-    	.week li {
-    		margin: 10px;
-    	}
-    	.week a {
-    		text-decoration: none;
-    		color: #2196f3;
-    		font-size: 20px;
-    	}
+      body {
+        font-family: "Cairo", sans-serif;
+        background-color: #ececec;
+      }
+      .card {
+        padding-right: 15px;
+        padding-left: 15px;
+        margin-right: auto;
+        margin-left: auto;
+      }
+      @media (min-width: 768px) {
+        .card {
+          width: 750px;
+        }
+      }
+      @media (min-width: 992px) {
+        .card {
+          width: 970px;
+        }
+      }
+      @media (min-width: 1200px) {
+        .card {
+          width: 1170px;
+        }
+      }
+      /* Start Head */
+
+      .head {
+        text-align: center;
+        padding: 25px 0;
+        background-color: #fafafa;
+        margin-bottom: 20px;
+      }
+      .head h2 {
+        font-size: 2em;
+        margin: 0;
+      }
+      /* End Head */
+      .week {
+        margin-bottom: 20px;
+      }
+      .week .box {
+        background-color: #fafafa;
+        padding: 10px 20px;
+        transition: 0.3s;
+        display: block;
+      }
+      .week .box:hover {
+        background-color: #e1e1e1;
+      }
+      .week .box {
+        margin-bottom: 20px;
+      }
+      .week .box {
+        font-size: 22px;
+        color: #000;
+        text-decoration: none;
+      }
     </style>
   </head>
   <body>
   	<div class="week">
-  		<h2>Week One</h2>
-  		<ul>
-  			<li><a href="week_one/assignment_1.php">Assignment 1</a></li>
-  			<li><a href="week_one/assignment_2.php">Assignment 2</a></li>
-  			<li><a href="week_one/assignment_3.php">Assignment 3</a></li>
-  			<li><a href="week_one/assignment_4.php">Assignment 4</a></li>
-  		</ul>
+      <div class="card">
+        <div class="head">
+          <h2>Week One</h2>
+        </div>
+        <div class="list">
+          <a href="week_one/assignment_1.php" class="box">Assignment 1</a>
+          <a href="week_one/assignment_2.php" class="box">Assignment 2</a>
+          <a href="week_one/assignment_3.php" class="box">Assignment 3</a>
+          <a href="week_one/assignment_4.php" class="box">Assignment 4</a>
+        </div>
+        <hr>
+      </div>
   	</div>
   </body>
 </html>
